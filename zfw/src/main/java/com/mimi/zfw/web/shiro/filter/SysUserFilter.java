5b75320370df1822aeac4fbc071082076c1c64e6
bluemimi <252443043@qq.com>
2015-04-17 23:24:19 +0800

    shiro新类

---
+++ zfw/src/main/java/com/mimi/zfw/web/shiro/filter/SysUserFilter.java
@@ -0,0 +1,30 @@
+package com.mimi.zfw.web.shiro.filter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.web.filter.PathMatchingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mimi.zfw.Constants;
+import com.mimi.zfw.service.IUserService;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-2-15
+ * <p>Version: 1.0
+ */
+public class SysUserFilter extends PathMatchingFilter {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+
+        String username = (String)SecurityUtils.getSubject().getPrincipal();
+        request.setAttribute(Constants.CURRENT_USER, userService.findByUsername(username));
+        return true;
+    }
+}
